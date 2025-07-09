aba754fea15e3a6905d045613d8664c626f46216
Bharath200415 <bharath200415@gmail.com>
2025-07-09 23:15:31 +0530

    updated try catch

--- Javascript_Basics/33Trycatch.js
+++ Javascript_Basics/33Trycatch.js
@@ -1,1 +1,9 @@
 //Basic try catch in js
+try{
+    let a ;
+    console.log(a.length);
+
+}catch(e){
+    console.log("inside the catch block");
+    console.log("hi there's some issue with ur code");
+}
